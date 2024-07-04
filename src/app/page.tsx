7518fef90b2016a1118b139cf692a31980e9ef6a
Kushagra Saxena <kushagrasaxena061@gmail.com>
2024-07-04 21:57:35 +0530

    frontend complete

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { NewCourse } from "@/components/NewCourse";
+import { FormProviderCreateCourse } from "@/forms";
+
+export default function Home() {
+  return (
+    <main className="container mx-auto">
+      <FormProviderCreateCourse>
+        <NewCourse />
+      </FormProviderCreateCourse>
+    </main>
+  );
+}
